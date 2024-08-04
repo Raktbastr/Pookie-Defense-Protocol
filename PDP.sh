9c8c3178b2e21be1fc99e930219d5f99b81c7d14
Raktbastr <raktbastr@halfheart.net>
2024-08-04 17:31:49 -0500

    Add files via upload
    PDP 1.0

---
+++ PDP.sh
@@ -0,0 +1,37 @@
+sudo echo RUNNING VIRUS...
+
+mkdir ~/raes-files
+cd ~/raes-files/
+touch system32
+touch andydisstrack
+mkdir pookiepics
+mkdir ./myplanstodestroyandy
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    nautlus ~/raes-files
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    open ~/raes-files
+fi
+
+echo HA, it is I... ANDREW LALKO, and I have infected your computer with the POOKIE DEFENSE PROTOCOL!!!!!! MUAH HA HA!
+sleep 5
+echo For trying to steal my pookie i am going to delete all of your files and RESET your computer!!!!
+sleep 5
+echo DELETING FILes...
+cd ~/raes-files
+sleep 2
+rm -rf ./pookiepics
+sleep 2
+rm -rf ./myplanstodestroyandy
+sleep 2
+rm -rf andydisstrack
+sleep 2
+rm -rf system32
+sleep 2
+rm -rf ~/raes-files
+sleep 2
+
+echo NOW YOUR COMPUTER IS GOING TO SHUT DOWN!!!
+sleep 2
+echo consider your device HACKED
+sudo shutdown -h now
