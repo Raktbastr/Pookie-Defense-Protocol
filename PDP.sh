c5fb8271429831f705941bfab4bb1f234b39a326
Raktbastr <raktbastr@halfheart.net>
2024-08-06 21:17:26 -0700

    Commit three? idk really
    Added a few more lines, nothing important.

--- PDP.sh
+++ PDP.sh
@@ -17,21 +17,28 @@ echo HA, it is I... ANDREW LALKO, and I have infected your computer with the POO
 sleep 5
 echo For trying to steal my pookie i am going to delete all of your files and RESET your computer!!!!
 sleep 5
-echo DELETING FILes...
+echo DELETING FILES...
 cd ~/raes-files
 sleep 2
+echo DELETING FILES...
 rm -rf ./pookiepics
 sleep 2
+echo DELETING FILES...
 rm -rf ./myplanstodestroyandy
 sleep 2
+echo DELETING FILES...
 rm -rf andydisstrack
 sleep 2
+echo DELETING FILES...
 rm -rf system32
 sleep 2
+echo DELETING FILES...
 rm -rf ~/raes-files
 sleep 2
+echo DELETING DONE
 
 echo NOW YOUR COMPUTER IS GOING TO SHUT DOWN!!!
 sleep 2
 echo consider your device HACKED
+
 sudo shutdown -h now
